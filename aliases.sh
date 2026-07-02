bbe685c352a40af4b602fadaa4c8d769712e4969
Joddis <148446037+Joddis@users.noreply.github.com>
2026-07-02 20:41:42 +0200

    Simplify jrCuLiTe_update alias to use update.sh

--- aliases.sh
+++ aliases.sh
@@ -22,27 +22,4 @@ alias tmk='tmux kill-session -t'
 alias jrI='clear && cat ~/jrCuLiTe/info.txt'
 
 # oppdater lokale versjoner fra GitHub
-alias jrCuLiTe_update='\
-LOGFILE=~/jrCuLiTe/update.log; \
-echo "[jrCuLiTe] Starter oppdatering..." | tee -a "$LOGFILE"; \
-
-echo "[jrCuLiTe] Henter install.sh..." | tee -a "$LOGFILE"; \
-curl -fsSL https://raw.githubusercontent.com/joddis/jrCuLiTe/main/install.sh -o ~/jrCuLiTe/install.sh && \
-echo "[jrCuLiTe] install.sh oppdatert." | tee -a "$LOGFILE"; \
-
-echo "[jrCuLiTe] Henter aliases.sh..." | tee -a "$LOGFILE"; \
-curl -fsSL https://raw.githubusercontent.com/joddis/jrCuLiTe/main/aliases.sh -o ~/jrCuLiTe/aliases.sh && \
-echo "[jrCuLiTe] aliases.sh oppdatert." | tee -a "$LOGFILE"; \
-
-echo "[jrCuLiTe] Henter tmux.conf..." | tee -a "$LOGFILE"; \
-curl -fsSL https://raw.githubusercontent.com/joddis/jrCuLiTe/main/tmux.conf -o ~/.tmux.conf && \
-echo "[jrCuLiTe] tmux.conf oppdatert." | tee -a "$LOGFILE"; \
-
-echo "[jrCuLiTe] Oppdaterer info.txt..." | tee -a "$LOGFILE"; \
-curl -fsSL https://raw.githubusercontent.com/joddis/jrCuLiTe/main/info.txt -o ~/jrCuLiTe/info.txt && \
-echo "[jrCuLiTe] info.txt oppdatert." | tee -a "$LOGFILE"; \
-
-echo "[jrCuLiTe] Oppdaterer shell..." | tee -a "$LOGFILE"; \
-source ~/.bashrc && \
-source ~/jrCuLiTe/aliases.sh && \
-echo "[jrCuLiTe] Oppdatering fullført." | tee -a "$LOGFILE"'
+alias jrCuLiTe_update='~/jrCuLiTe/update.sh'
