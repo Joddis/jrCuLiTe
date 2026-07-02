87a33c2b93e25518d49ce53b2c439d8cb7ec81fb
Joddis <148446037+Joddis@users.noreply.github.com>
2026-07-02 20:40:27 +0200

    Add update script for jrCuLiTe project

---
+++ update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+LOGFILE=~/jrCuLiTe/update.log
+
+echo "[jrCuLiTe] Starter oppdatering..." | tee -a "$LOGFILE"
+
+TMPDIR=~/jrCuLiTe-temp
+mkdir -p "$TMPDIR"
+
+# --- Last ned alle filer fra GitHub ---
+echo "[jrCuLiTe] Henter siste versjon fra GitHub..." | tee -a "$LOGFILE"
+git clone https://github.com/joddis/jrCuLiTe.git "$TMPDIR"
+
+# --- Kopier filer ---
+echo "[jrCuLiTe] Oppdaterer lokale filer..." | tee -a "$LOGFILE"
+cp "$TMPDIR"/*.sh ~/jrCuLiTe/
+cp "$TMPDIR"/tmux.conf ~/.tmux.conf
+cp "$TMPDIR"/aliases.sh ~/jrCuLiTe/aliases.sh
+cp "$TMPDIR"/info.txt ~/jrCuLiTe/info.txt
+
+# --- Rydd opp ---
+rm -rf "$TMPDIR"
+
+# --- Sett +x på alle scripts ---
+echo "[jrCuLiTe] Setter kjørbarhet på scripts..." | tee -a "$LOGFILE"
+chmod +x ~/jrCuLiTe/*.sh
+
+# --- Reload shell ---
+echo "[jrCuLiTe] Oppdaterer shell..." | tee -a "$LOGFILE"
+source ~/.bashrc
+source ~/jrCuLiTe/aliases.sh
+
+echo "[jrCuLiTe] Oppdatering fullført." | tee -a "$LOGFILE"
