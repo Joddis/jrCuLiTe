a46809cc5c6eb6d1f0eb82ddd644413bc5798d1b
Joddis <148446037+Joddis@users.noreply.github.com>
2026-07-07 17:56:23 +0200

    Refactor aliases.sh to remove duplicates
    Removed duplicate alias definitions and organized the file.

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,19 @@
 # jrCuLiTe alias-fil
 ####################
 
+# === jrCuLiTe shortcuts commands ===
+alias jrI='clear && cat ~/jrCuLiTe/info.txt'   # vis min nyttige info/tips/triks notat
+alias jrCuLiTe_update='~/jrCuLiTe/update.sh'   # oppdater lokale versjoner fra GitHub
+
+# === egne kommandoer som kjører egne scripts ===
+alias jrDocStat='clear && docker ps --format "table {{.Names}}\t{{.Status}}"'   # viser egenversjon av docker stats
+alias jrCleanHDD='clear && sudo ~/jrCuLiTe/jrCleanHDD.sh'                       # rydder søppel fra disken
+
+# === TMUX ===
+alias tm='tmux attach || tmux new'
+alias tml='tmux ls'
+alias tmk='tmux kill-session -t'
+
 # system
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -9,17 +22,3 @@ alias update='sudo apt update && sudo apt upgrade -y'
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
-
-# docker
-alias jrDocStat='clear && docker ps --format "table {{.Names}}\t{{.Status}}"'
-
-# tmux quality-of-life
-alias tm='tmux attach || tmux new'
-alias tml='tmux ls'
-alias tmk='tmux kill-session -t'
-
-# vis min nyttige info/tips/triks notat
-alias jrI='clear && cat ~/jrCuLiTe/info.txt'
-
-# oppdater lokale versjoner fra GitHub
-alias jrCuLiTe_update='~/jrCuLiTe/update.sh'
